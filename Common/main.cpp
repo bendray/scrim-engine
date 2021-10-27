df35dc4ec6c59d23116b5f6b08c21ed593d1230f
bendray <mikh4il@gmail.com>
2021-10-28 00:59:27 +0300

    Main

--- Common/main.cpp
+++ Common/main.cpp
@@ -1,14 +1,10 @@
-#include "Application.hpp"
+#include "Launcher.hpp"
 #if defined(MSVC) 
 int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd )
 #else
 int main()
 #endif
 {
-	Application app;
-	if(app.Load())
-	{
-		return app.Run();
-	}
-	return -1;
+	CLauncher app;
+	return app.Execute();
 }
