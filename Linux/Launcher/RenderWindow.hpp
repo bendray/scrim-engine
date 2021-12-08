9a601ef5eb116a9636e26362d003dd0a3fb41b70
bendray <mikh4il@gmail.com>
2021-12-08 21:40:27 +0300

    Linux support.

--- Linux/Launcher/RenderWindow.hpp
+++ Linux/Launcher/RenderWindow.hpp
@@ -15,79 +15,79 @@ extern XVisualInfo* g_pVisualInfo;
 class RenderWindow : public Scrim::IRenderWindow
 {
 public:
-	RenderWindow();
-	//RenderWindow(RenderWindow& other);
-	~RenderWindow(void);
-	virtual void Update();
-	virtual void SetCaption(const Scrim::UString& caption);
-	virtual void SetSize(int w, int h);
-	virtual void CenterWindow();
+    RenderWindow();
+    //RenderWindow(RenderWindow& other);
+    ~RenderWindow(void);
+    virtual void Update();
+    virtual void SetCaption(const Scrim::UString& caption);
+    virtual void SetSize(int w, int h);
+    virtual void CenterWindow();
 
-	virtual size_t GetWindowHandle() { return (size_t)m_window; }
+    virtual size_t GetWindowHandle() { return (size_t)m_window; }
 
-	virtual int GetWidth()
-	{
-		if(!IsFullScreen())
-			return m_width;
-		else
-			return m_mode_fullscreen.width;
-	}
+    virtual int GetWidth()
+    {
+        if(!IsFullScreen())
+            return m_width;
+        else
+            return m_mode_fullscreen.width;
+    }
 
-	virtual int GetHeight()
-	{
-		if(!IsFullScreen())
-			return m_height;
-		else
-			return m_mode_fullscreen.height;
-	}
+    virtual int GetHeight()
+    {
+        if(!IsFullScreen())
+            return m_height;
+        else
+            return m_mode_fullscreen.height;
+    }
 
-	virtual void OnResize()
-	{
-		if(!IsFullScreen() || !m_bModeSwitching)
-		{
-			
-		}
-		Scrim::IRenderWindow::OnResize();
-	}
+    virtual void OnResize()
+    {
+        if(!IsFullScreen() || !m_bModeSwitching)
+        {
+            
+        }
+        Scrim::IRenderWindow::OnResize();
+    }
 
-	bool SwitchMode(bool fullscreen);
-	void SetFullscreenMode(Scrim::DisplayMode mode);
+    bool SwitchMode(bool fullscreen);
+    void SetFullscreenMode(Scrim::DisplayMode mode);
 
-	bool IsFullScreen()
-	{
-		return m_bFullscreen;
-	}
-	const Scrim::DisplayMode& GetFullscreenMode()
-	{
-		return m_mode_fullscreen;
-	}
-	bool IsModeSwitching()
-	{
-		return m_bModeSwitching;
-	}
-	void SetAltEnterLocked(bool state)
-	{
-		m_bAltEnterLocked = state;
-	}
-	bool IsAltEnterLocked()
-	{
-		return m_bAltEnterLocked;
-	}
+    bool IsFullScreen()
+    {
+        return m_bFullscreen;
+    }
+    const Scrim::DisplayMode& GetFullscreenMode()
+    {
+        return m_mode_fullscreen;
+    }
+    bool IsModeSwitching()
+    {
+        return m_bModeSwitching;
+    }
+    void SetAltEnterLocked(bool state)
+    {
+        m_bAltEnterLocked = state;
+    }
+    bool IsAltEnterLocked()
+    {
+        return m_bAltEnterLocked;
+    }
 
 protected:
-	Scrim::DisplayMode m_mode_fullscreen;
-	bool        m_bFullscreen;
-	bool        m_bModeSwitching;
-	bool        m_bAltEnterLocked;
+    Scrim::DisplayMode m_mode_fullscreen;
+    bool        m_bFullscreen;
+    bool        m_bModeSwitching;
+    bool        m_bAltEnterLocked;
 
-	Display* m_pDisplay;
-	XVisualInfo* m_visualInfo;
+    Display* m_pDisplay;
+    XVisualInfo* m_visualInfo;
 
-	XSetWindowAttributes m_frameAttributes;
-	Window               m_window;
+    XSetWindowAttributes m_frameAttributes;
+    Window               m_window;
 
-	Atom m_wmDeleteWindow;
+    Atom m_wmDeleteWindow;
 
-	XKeyEvent m_event;
+    XKeyEvent m_event;
 
 };
